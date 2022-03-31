050967fd6830e4dbafa4a2ea67b35a75ae76531d
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2022-03-31 11:11:10 +0200

    Added highscore and info page boilerplate

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs/promises';
+import { engine } from 'express-handlebars';
 import { fetchRandomWord } from './words.js';
 import { Highscore } from './database.js';
+import console from 'console';
+
 const app = express();
 const port = 5080;
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './server/templates');
 app.use(express.json());
 app.use(cors());
 
@@ -15,8 +22,18 @@ app.get('/api/word', async (req, res) => {
 });
 
 app.get('/api/highscores', async (req, res) => {
-  const highscores = await Highscore.find();
-  res.json(highscores);
+  const getHighscores = await Highscore.find();
+  const highscoresList = getHighscores.map((item) => ({
+    name: item.name,
+    score: item.score,
+  }));
+  res.render('home', { highscoresList });
+});
+
+app.get('/api/info', async (req, res) => {
+  const fileBuf = await fs.readFile('./server/static/static.html');
+  res.type('html');
+  res.send(fileBuf);
 });
 
 //POST request
@@ -26,4 +43,15 @@ app.post('/api/highscores', async (req, res) => {
   res.json(postHighscores, 201);
 });
 
+app.use('/api/info', express.static('./static'));
 app.listen(port, () => console.log(`Listening to port ${port}`));
+
+/* 
+1. In home.handlebars, render highscore from mongodb atlas
+2. 
+  A: Fetch everything from mongodb
+  B: Send data to home.handlebars
+  C: Loop through data and render with ul/li
+  D: Style the shit out of it until perfection
+
+*/
